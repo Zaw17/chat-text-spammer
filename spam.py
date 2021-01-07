8023aac4a3ace32f501dbddc5404410773d21045
John Zaw <johnzaw17@gmail.com>
2021-01-07 15:04:09 +0630

    added some buttons

--- spam.py
+++ spam.py
@@ -2,6 +2,7 @@ import pyautogui, time
 import os
 import tkinter as tk 
 
+
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 root= tk.Tk()
@@ -15,6 +16,21 @@ label1 = tk.Label(root, text='Spam text from file')
 label1.config(font=('helvetica', 14))
 canvas1.create_window(200, 25, window=label1)
 
+v = tk.StringVar(root, "1")
+
+values = {"txt" : ".txt", 
+          "docx" : ".docx", 
+          "rtf" : ".rtf", 
+          }  
+
+label3 = tk.Label(root, text = "Select file type")
+label3.config(font=('helvetica', 14))
+canvas1.create_window(200,300 , window=label3)
+
+for (text, value) in values.items(): 
+    tk.Radiobutton(root, text = text, variable = v, 
+        value = value).pack(side = tk.TOP, ipady = 5)
+
 label2 = tk.Label(root, text="Type in the file name that's in the same folder")
 label2.config(font=('helvetica', 10))
 canvas1.create_window(200, 100, window=label2)
@@ -24,12 +40,8 @@ canvas1.create_window(200, 140, window=entry1)
 
 def write():
 	x1 = entry1.get()
-
-	label3 = tk.Label(root, text="Spamming text from" + x1)
-	label3.config(font=('helvetica', 10))
-	canvas1.create_window(200, 100, window=label3)
-
-	f = open(dir_path + "/" + str(x1) + ".txt",'r')
+	selection = str(v.get())
+	f = open(dir_path + "/" + str(x1) + selection,'r')
 
 	time.sleep(10)
 
