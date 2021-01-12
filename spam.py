b48e282c97b8f9426766e88dd19dc2d8407bfc8e
John Zaw <johnzaw17@gmail.com>
2021-01-12 22:57:50 +0630

    added drop down list and support for rtf and docx

--- spam.py
+++ spam.py
@@ -5,38 +5,50 @@ import tkinter as tk
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-root= tk.Tk()
+root = tk.Tk()
+
+root.resizable(0,0)
 
 root.title("Spammer")
 
-canvas1 = tk.Canvas(root, width = 400, height = 300,  relief = 'raised')
+canvas1 = tk.Canvas(root, width = 400, height = 310,  relief = 'raised', bg="#0059b3")
 canvas1.pack()
 
-label1 = tk.Label(root, text='Spam text from file')
+label1 = tk.Label(root, text='Spam text from file', bg="#0059b3")
 label1.config(font=('helvetica', 14))
 canvas1.create_window(200, 25, window=label1)
 
-v = tk.StringVar(root, "1")
-
-values = {"txt" : ".txt", 
-          "docx" : ".docx", 
-          "rtf" : ".rtf", 
-          }  
-
-label3 = tk.Label(root, text = "Select file type")
-label3.config(font=('helvetica', 14))
-canvas1.create_window(200,300 , window=label3)
-
-for (text, value) in values.items(): 
-    tk.Radiobutton(root, text = text, variable = v, 
-        value = value).pack(side = tk.TOP, ipady = 5)
-
-label2 = tk.Label(root, text="Type in the file name that's in the same folder")
-label2.config(font=('helvetica', 10))
-canvas1.create_window(200, 100, window=label2)
+label2 = tk.Label(root, text="Type in the file name that's in the same folder", bg="#0059b3")
+label2.config(font=('helvetica', 14))
+canvas1.create_window(200, 140, window=label2)
 
 entry1 = tk.Entry(root)
-canvas1.create_window(200, 140, window=entry1)
+canvas1.create_window(200, 170, window=entry1)
+
+ftypes=[".txt", 
+    	".docx", 
+        ".rtf", 
+		]  
+
+v = tk.StringVar(root)
+v.set(ftypes[0])
+
+label3 = tk.Label(root, text = "Select file type", bg="#0059b3")
+label3.config(font=('helvetica', 14))
+canvas1.create_window(200,100 , window=label3)
+
+opt = tk.OptionMenu(root, v, *ftypes)
+opt.config(width=10, font=('Helvetica', 12), bg="#0059b3")
+canvas1.create_window(200,120 , window= opt)
+
+label5 = tk.Label(root, text= "Move your mouse cursor to the top right in order to cancel the process", bg="#0059b3")
+label5.config(font=('helvetica', 10))
+canvas1.create_window(200,250, window=label5)
+
+
+label5 = tk.Label(root, text= "©Myo Khant Zaw 2020", bg="#0059b3")
+label5.config(font=('helvetica', 10))
+canvas1.create_window(200,300, window=label5)
 
 def write():
 	x1 = entry1.get()
@@ -49,10 +61,16 @@ def write():
 		pyautogui.typewrite(word)
 		pyautogui.press('return')
 
-	
+labelTest = tk.Label(text="", font=('Helvetica', 12), fg='red', bg="#0059b3")
+labelTest.pack(side="top")
+canvas1.create_window(200,280, window=labelTest)
 
-button1 = tk.Button(text='Spam the text', command=write, bg='blue', fg='white', font=('helvetica', 9, 'bold'))
-canvas1.create_window(200, 180, window=button1)
+def callback(*args):
+	labelTest.configure(text="Will spam " +entry1.get() + str(v.get()))
+v.trace("w", callback)
+
+button1 = tk.Button(text='Spam the text', command=write, bg='white', fg='black', font=('helvetica', 9, 'bold'))
+canvas1.create_window(200, 200, window=button1)
 
 root.mainloop()
 
